6323a96542fb2f2bc1d38b0f9874ff7df975cce5
Tom Dyer <tdyer1@gmail.com>
2015-05-04 19:00:13 -0400

    Finish app

---
+++ app/js/app_done.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  // trace('hello world');
+  Spotify.PlayList.init($('#spotify-songs'));
+  Spotify.PlayList.render();
+});
+
+
---
+++ app/js/play_list_done.js
@@ -0,0 +1,44 @@
+// This will create one global variable for our app.
+// It will act as a namespace for our application
+var Spotify = Spotify || {};
+
+// Constructor function for a Playlist
+// ONLY ONE PLAYLIST IN THIS APP, SO IT'S AN OBJECT LITERAL
+Spotify.PlayList = (function(){
+
+  // private variable, array that contains all the songs
+  var _songs = [], 
+  // The HTML DOM Element for the playlist
+  $playListElement;
+
+  // set the HTML Element for the playlist
+  function _init(appPlayListElement){
+    $playListElement = appPlayListElement;
+
+    // Will eventually be a remote call to get songs from the server 
+    _getSongs();
+  };
+
+  // Render each song into HTML
+  function _render(){
+    _songs.forEach(function(song){
+      song.render($playListElement);
+    }); 
+  }; 
+
+  // Dummy function that creates a initial set of songs.
+  // We'll get these songs from the backend server later
+  function _getSongs(){
+    var song = 
+    _songs.push(new Spotify.Song("Lost Cause", 183, 1.99, 'Beck'));
+    _songs.push(new Spotify.Song("Teenage spirit", 243, 1.09,'Nivarna'));
+    _songs.push(new Spotify.Song("Whole lotta love", 203, 2.99, 'Zeppelin'));
+    _songs.push(new Spotify.Song("Mother", 605, 1.49,'Pink Floyd'));
+  };
+  
+  // Return the Object Literal for client code
+  return {
+    init: _init,
+    render: _render
+  }
+})();
